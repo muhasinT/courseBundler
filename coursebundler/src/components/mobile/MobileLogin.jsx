06371ee808d349e8513c9984794007b3e9425498
muhasin <muhasin.moideen@gmail.com>
2023-05-11 13:34:14 +0530

    production deploy

--- coursebundler/src/components/mobile/MobileLogin.jsx
+++ coursebundler/src/components/mobile/MobileLogin.jsx
@@ -2,73 +2,60 @@ import { Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { mobilelogin } from '../../redux/actions/user';
-import { useNavigate } from 'react-router-dom';
-import  toast  from 'react-hot-toast';
+import {useNavigate} from 'react-router-dom';
 
 const MobileLogin = () => {
 
-    const [number, setNumber] = useState("");
+    const [number,setNumber] = useState("");
 
     const [returnNumber, setReturnNumber] = useState("");
 
     const userNumber = useSelector((state) => state.user.number);
 
-    const { message, error } = useSelector((state) => state.user);
+    const navigate = useNavigate();
 
-    useEffect(() => {
+    useEffect(()=>{
         setReturnNumber(userNumber);
-        if (returnNumber) {
+        if(returnNumber){
             navigate("/mobilelogin/verify")
         }
+    },[navigate,returnNumber,userNumber]);
 
-        if (error) {
-            toast.error(error);
-            dispatch({ type: 'clearError' });
-        }
-
-        if (message) {
-            toast.success(message);
-            dispatch({ type: 'clearMessage' });
-        }
-    }, [dispatch, error, message]);
-
-    const navigate = useNavigate();
 
     const dispatch = useDispatch();
 
     const submitHandler = (e) => {
         e.preventDefault();
         dispatch(mobilelogin(number));
-
+       
     };
-
-
-    return (
-        <Container py={'16'} h={'85vh'}>
-            <form onSubmit={submitHandler}>
-                <Heading
-                    children="Login With Mobile Number"
-                    my="16"
-                    textTransform={"uppercase"}
-                    textAlign={['center', 'left']}
+       
+  return (
+    <Container py={'16'} h={'85vh'}>
+        <form onSubmit={submitHandler}>
+            <Heading
+            children="Login With Mobile Number"
+            my="16"
+            textTransform={"uppercase"}
+            textAlign={['center','left']}
+            />
+            <VStack spacing={'8'}>
+                <Input 
+                required
+                value={number}
+                onChange={e => setNumber(e.target.value)}
+                placeholder='Enter your Mobile Number'
+                type='number'
+                focusBorderColor="red.500"
                 />
-                <VStack spacing={'8'}>
-                    <Input
-                        required
-                        value={number}
-                        onChange={e => setNumber(e.target.value)}
-                        placeholder='Enter your Mobile Number'
-                        type='number'
-                        focusBorderColor="red.500"
-                    />
-                </VStack>
-                <Button type='submit' colorScheme='red' my={'4'}>
-                    OTP Generate
-                </Button>
-            </form>
-        </Container>
+            </VStack>
+            <Button type='submit' colorScheme='red' my={'4'}>
+                OTP Generate
+            </Button>
+        </form>
+    </Container>
 
-    )
+  )
 };
 
 export default MobileLogin
